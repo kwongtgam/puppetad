196d2bdede690263d8a557addebdec3966a8c508
Ken Wong <kenbong@gmail.com>
2014-01-21 14:51:40 -0500

    Change install to exec

--- immio/manifests/sbt.pp
+++ immio/manifests/sbt.pp
@@ -20,13 +20,22 @@ class immio::sbt(
   }
   ->
   # Unpack SBT
-  package { 'sbt':
-    provider => dpkg,
-    ensure   => installed,
-    install_options => [ '--ignore-depends=java2-runtime' ],
-    source   => "/opt/$pkgname",
+  #package { 'sbt':
+  #  provider => dpkg,
+  #  ensure   => installed,
+  #  install_options => [ '--ignore-depends=java2-runtime' ],
+  #  source   => "/opt/$pkgname",
+  #}
+ # ->
+
+  # Install SBT Deb package
+  exec { 'install-sbt':
+    command => "dpkg -i --ignore-depends=java2-runtime /opt/$pkgname ; touch /usr/local/src/.sbtdownloaded",
+    unless  => "dpkg-query -l sbt",
+    #creates => "/usr/local/src/.sbtdownloaded",
   }
  # ->
+
   # Update JDK Alternatives
  # exec { 'update-java-alt':
  #   command => "update-alternatives --install /usr/bin/java java /opt/${unpackdir}/bin/java 20000",
